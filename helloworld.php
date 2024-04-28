71ed108525bea955311cf7a27d056a2d83edb1ed
Syifaaurellia18 <syifarrahma2@gmail.com>
2024-04-28 13:25:20 +0700

    add helloworld.php

---
+++ helloworld.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "hello world";
+
+// variable
+// camelcase
+$jumlahKunatitas = 10;
+
+// snake_case
+$jumlah_kuantitas = 10;
+
+echo "snake case variable: " . $jumlah_kuantitas, "camel case variable: " . $jumlahKunatitas;
